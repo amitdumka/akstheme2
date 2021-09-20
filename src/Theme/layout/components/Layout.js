af4d8f43956742693b24efa752d3d527c322600b
Amit Kumar <amitnarayansah@gmail.com>
2021-09-20 11:41:06 +0530

    Layout Init is ported to Class.

--- src/Theme/layout/components/Layout.js
+++ src/Theme/layout/components/Layout.js
@@ -1,7 +1,7 @@
 import React, { useMemo } from "react";
 import objectPath from "object-path";
 // LayoutContext
-import { useHtmlClassService } from "../_core/MetronicLayout";
+import { useHtmlClassService } from "../_core/Layout";
 // Import Layout components
 import { Header } from "./header/Header";
 import { HeaderMobile } from "./header-mobile/HeaderMobile";
@@ -13,6 +13,96 @@ import { QuickPanel } from "./extras/offcanvas/QuickPanel";
 import { QuickUser } from "./extras/offcanvas/QuickUser";
 import { ScrollTop } from "./extras/ScrollTop";
 import { StickyToolbar } from "./extras/StickyToolbar";
+
+export class LayoutUI extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      layoutConfig: null,
+      selfLayout: null,
+      asideDisplay: null,
+      subheaderDisplay: null,
+      desktopHeaderDisplay: null,
+      contentCssClasses: null,
+      contentContainerClasses: null,
+      contentExtended: null,
+    };
+    const uiService = useHtmlClassService();
+  }
+  componentDidMount() {
+    // Call all Init variable and loading of data
+    this.setState({
+      layoutConfig: uiService.config,
+      selfLayout: objectPath.get(uiService.config, "self.layout"),
+      asideDisplay: objectPath.get(uiService.config, "aside.self.display"),
+      subheaderDisplay: objectPath.get(uiService.config, "subheader.display"),
+      desktopHeaderDisplay: objectPath.get(
+        uiService.config,
+        "header.self.fixed.desktop"
+      ),
+      contentCssClasses: uiService.getClasses("content", true),
+      contentContainerClasses: uiService.getClasses("content_container", true),
+      contentExtended: objectPath.get(uiService.config, "content.extended"),
+    });
+  }
+
+  componentDidUpdate() {
+    //call all function when there is update in state and rending happing.
+  }
+  render() {
+    if (this.state.selfLayout !== "blank")
+      return (
+        <>
+          {/*begin::Main*/}
+          <HeaderMobile />
+          <div className="d-flex flex-column flex-root">
+            {/*begin::Page*/}
+            <div className="d-flex flex-row flex-column-fluid page">
+              {layoutProps.asideDisplay && <Aside />}
+              {/*begin::Wrapper*/}
+              <div
+                className="d-flex flex-column flex-row-fluid wrapper"
+                id="kt_wrapper"
+              >
+                <Header />
+                {/*begin::Content*/}
+                <div
+                  id="kt_content"
+                  className={`content ${layoutProps.contentCssClasses} d-flex flex-column flex-column-fluid`}
+                >
+                  {layoutProps.subheaderDisplay && <SubHeader />}
+                  {/*begin::Entry*/}
+                  {!layoutProps.contentExtended && (
+                    <div className="d-flex flex-column-fluid">
+                      {/*begin::Container*/}
+                      <div className={layoutProps.contentContainerClasses}>
+                        {children}
+                      </div>
+                      {/*end::Container*/}
+                    </div>
+                  )}
+
+                  {layoutProps.contentExtended && { children }}
+                  {/*end::Entry*/}
+                </div>
+                {/*end::Content*/}
+                <Footer />
+              </div>
+              {/*end::Wrapper*/}
+            </div>
+            {/*end::Page*/}
+          </div>
+          <QuickUser />
+          <QuickPanel />
+          <ScrollTop />
+          <StickyToolbar />
+          {/*end::Main*/}
+          <LayoutInit />
+        </>
+      );
+    else return <div className="d-flex flex-column flex-root">{children}</div>;
+  }
+}
 
 export function Layout({ children }) {
   const uiService = useHtmlClassService();
